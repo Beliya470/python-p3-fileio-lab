46bffc10a0dd4d022185815a8d7e46a6f05313e1
Beliya470 <anneanziay78@gmail.com>
2023-09-07 17:59:24 +0300

    Completed Assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
-def write_file(file_name, file_content):
-    pass
+# File: file_io.py
 
-def append_file(file_name, append_content):
-    pass
 
-def read_file(file_name):
-    pass
+def write_file(file_path, file_content):
+    with open(file_path, "w") as file:
+        file.write(file_content)
+
+def append_file(file_path, append_content):
+    with open(file_path, "a+") as file:
+        file.seek(0)
+        content = file.read()
+        # Only append a newline if the file has content
+        if content:
+            file.write("\n" + append_content)
+        else:
+            file.write(append_content)
+
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
